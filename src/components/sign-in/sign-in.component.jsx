9e7a9dcdcb0b46530f62fe878c7e9f805c299178
Cesar Guajardo <guajardoedahi@gmail.com>
2022-02-21 11:51:20 -0600

    native sign up added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import './signin.styles.scss'
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component'
 import {signInWithGoogle} from '../../firebase/firebase.utils'
+
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore'
 
 const config = {
     apiKey: "AIzaSyAdVbiHMPc1fpbSNMa_eboi7Q8PuD9pSCY",
@@ -11,6 +12,36 @@ const config = {
     appId: "1:1094749977082:web:8f0a0a4fd0bd963b81cc43",
     measurementId: "G-D1GR04QZ8X"
   };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot = await userRef.get()
+
+    if(!snapShot.exists){
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+      try{
+
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+
+      }catch(error){
+        console.log('error creating user', error.message);
+      }
+
+
+    }return userRef;
+   
+
+  }
 
   firebase.initializeApp(config);
 
